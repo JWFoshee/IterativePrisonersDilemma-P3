be53e70282b4c6537c8661b635aae24afd9dad31
Eric <ericg214@gmail.com>
2017-01-04 10:49:17 -0600

    Team 0 Names
    Team and individual names for team 0

--- team0.py
+++ team0.py
@@ -6,9 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Reciprocation'
+strategy_description = "Copies the partner's move from the previous iteration."
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
